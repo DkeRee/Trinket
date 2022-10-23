ab2272de0b32d1f9ca1358b2f91923fd9169cc03
DkeRee <dkeree1@gmail.com>
2022-10-23 10:08:54 -0700

    Testing...

--- src/search/searcher.rs
+++ src/search/searcher.rs
@@ -18,6 +18,7 @@ pub struct Searcher<'a> {
 	pub time_control: TimeControl,
 	pub shared_info: &'a SharedInfo<'a>,
 	pub movegen: MoveGen,
+	best_move: Option<Move>,
 	nodes: u64,
 	searching_depth: i32,
 	board: Board,
@@ -25,11 +26,12 @@ pub struct Searcher<'a> {
 }
 
 impl Searcher<'_> {
-	pub fn create(time_control: TimeControl, shared_info: &SharedInfo, movegen: MoveGen, board: Board, my_past_positions: Vec<u64>, handler: Option<Arc<AtomicBool>>) -> (MoveGen, u64) {
+	pub fn create(time_control: TimeControl, shared_info: &SharedInfo, movegen: MoveGen, board: Board, my_past_positions: Vec<u64>, handler: Option<Arc<AtomicBool>>) -> (MoveGen, u64, Option<Move>) {
 		let mut instance = Searcher {
 			time_control: time_control,
 			shared_info: shared_info,
 			movegen: movegen,
+			best_move: None,
 			nodes: 0,
 			searching_depth: 0,
 			board: board,
@@ -37,7 +39,7 @@ impl Searcher<'_> {
 		};
 
 		instance.go(handler.unwrap());
-		(instance.movegen, instance.nodes)
+		(instance.movegen, instance.nodes, instance.best_move)
 	}
 
 	fn go(&mut self, handler: Arc<AtomicBool>) {
@@ -104,6 +106,9 @@ impl Searcher<'_> {
 						let mut best_move = self.shared_info.best_move.lock().unwrap();
 						let mut best_depth = self.shared_info.best_depth.lock().unwrap();
 
+						self.best_move = best_mv.clone();
+
+/*
 						if self.searching_depth > *best_depth {
 							*best_move = best_mv.clone();
 							*best_depth += 1;
@@ -111,6 +116,7 @@ impl Searcher<'_> {
 							//do not print out anything if we are searching at a lower depth than the current shared best depth
 							continue;
 						}
+*/
 					}
 
 					let nodes = self.nodes;

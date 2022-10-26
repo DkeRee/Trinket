188f98f8a525bd61a53c1946a2cc6f4eefd46c56
DkeRee <dkeree1@gmail.com>
2022-10-25 23:44:10 -0700

    Revert

--- src/search/search_master.rs
+++ src/search/search_master.rs
@@ -108,6 +108,32 @@ impl Engine<'_> {
 				}));
 			}
 
+			//manage time
+			let abort = handler.clone();
+
+			let mut time: f32;
+			let mut timeinc: f32;
+
+			//set time
+			match self.board.side_to_move() {
+				Color::White => {
+					time = time_control.wtime as f32;
+					timeinc = time_control.winc as f32;
+				},
+				Color::Black => {
+					time = time_control.btime as f32;
+					timeinc = time_control.binc as f32;	
+				}
+			}
+
+			//loop until allocated time is up
+			let now = Instant::now();
+			let mut elapsed = 0_f32;
+			while (elapsed < (time + timeinc) / f32::min(40_f32, time_control.movestogo as f32)) && !abort.load(Ordering::Relaxed) {
+				elapsed = now.elapsed().as_secs_f32() * 1000_f32;
+			}
+			abort.store(true, Ordering::Relaxed);
+
 			let mut index = 0;
 			for worker in worker_threads {
 				let (movegen, nodes) = worker.join().unwrap();
--- src/search/searcher.rs
+++ src/search/searcher.rs
@@ -1,7 +1,6 @@
 use cozy_chess::*;
 
-use std::thread;
-use std::time::{Instant, Duration};
+use std::time::Instant;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::Arc;
 
@@ -71,13 +70,6 @@ impl Searcher<'_> {
 
 				let board = &mut self.board.clone();
 
-				//set up multithread for search abort
-				let abort = handler.clone();
-				thread::spawn(move || {
-					thread::sleep(Duration::from_millis(time as u64 / 32));
-					abort.store(true, Ordering::Relaxed);
-				});
-
 				let mut past_positions = self.my_past_positions.clone();
 
 				let search_handler = handler.clone();
@@ -144,6 +136,8 @@ impl Searcher<'_> {
 				break;
 			}
 		}
+
+		handler.store(true, Ordering::Relaxed);
 	}
 
 	//fish PV from TT

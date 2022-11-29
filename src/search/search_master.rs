36e98f790f570e6b9e7ad8981d7048af5bfe3c5c
DkeRee <dkeree1@gmail.com>
2022-11-28 17:19:39 -0800

    Fix PV Bug

--- src/search/search_master.rs
+++ src/search/search_master.rs
@@ -164,7 +164,7 @@ impl Engine {
 
 	//fish PV from TT
 	fn get_pv(&self, board: &mut Board, depth: i32, ply: i32) -> String {
-		if depth == 0 || ply > 50 {
+		if depth == 0 || ply > 30 {
 			return String::new();
 		}
 

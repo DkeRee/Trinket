ba0d4536de6a25ecc58577b344a7fb902f9140ae
DkeRee <dkeree1@gmail.com>
2022-12-30 18:24:47 -0800

    remove redundancy

--- src/search/search_master.rs
+++ src/search/search_master.rs
@@ -559,8 +559,6 @@ impl Engine {
 	const NMP_YSTRETCH: i32 = 4;
 	const LMR_DEPTH_LIMIT: i32 = 3;
 	const LMR_FULL_SEARCHED_MOVE_LIMIT: i32 = 4;
-	const LMR_REDUCTION_BASE: f32 = 0.75;
-	const LMR_MOVE_DIVIDER: f32 = 2.25;
 	const IID_DEPTH_MIN: i32 = 6;
 	const LMP_DEPTH_MAX: i32 = 3;
 	const LMP_MULTIPLIER: i32 = 10;

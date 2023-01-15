a4309a976cb75e93f9d96c79913e523dc650d8d1
DkeRee <dkeree1@gmail.com>
2023-01-15 11:38:36 -0800

    Remove min depth for history leaf reductions

--- src/search/search_master.rs
+++ src/search/search_master.rs
@@ -395,17 +395,28 @@ impl Engine {
 				//get initial value with reduction and pv-search null window
 				let mut new_depth = depth;
 
-				//History Leaf Reduction/Pruning
-				//IF sufficient depth
+				//History Leaf Reduction
 				//IF ISNT PV
 				//IF ISNT in check
+				//IF sufficient moves searched
 				//IF ISNT extended
-				if depth >= Self::HISTORY_DEPTH_MIN && !is_pv && !in_check && moves_searched >= Self::HISTORY_PRUNE_MOVE_LIMIT && !extended {
+				if !is_pv && !in_check && moves_searched >= Self::HISTORY_PRUNE_MOVE_LIMIT && !extended {
 					let history_value = sm.history;
 
-					//History Leaf Reduction
 					if history_value < Self::HISTORY_THRESHOLD {
 						new_depth -= Self::HISTORY_REDUCTION;
+/*
+						//History Leaf Pruning
+						//IF new depth on reductions is less or equal to an amount
+						if new_depth <= Self::HISTORY_PRUNE_THRESHOLD {
+							let history_value = sm.history;
+
+							if history_value < Self::HISTORY_PRUNE_AMOUNT {
+								past_positions.pop();
+								continue;
+							}
+						}
+*/
 					}
 				}
 
@@ -590,8 +601,9 @@ impl Engine {
 	const IID_DEPTH_MIN: i32 = 6;
 	const LMP_DEPTH_MAX: i32 = 3;
 	const LMP_MULTIPLIER: i32 = 10;
-	const HISTORY_DEPTH_MIN: i32 = 5;
 	const HISTORY_PRUNE_MOVE_LIMIT: i32 = 5;
 	const HISTORY_THRESHOLD: i32 = 100;
+	const HISTORY_PRUNE_AMOUNT: i32 = 0;
+	const HISTORY_PRUNE_THRESHOLD: i32 = 0;
 	const HISTORY_REDUCTION: i32 = 1;
 }

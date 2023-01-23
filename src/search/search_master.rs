43647f81198e714c97b45562b295a2453312df78
DkeRee <dkeree1@gmail.com>
2023-01-22 17:14:39 -0800

    Tweak history depth

--- src/search/search_master.rs
+++ src/search/search_master.rs
@@ -594,7 +594,7 @@ impl Engine {
 	const IID_DEPTH_MIN: i32 = 6;
 	const LMP_DEPTH_MAX: i32 = 3;
 	const LMP_MULTIPLIER: i32 = 10;
-	const HISTORY_DEPTH_MIN: i32 = 6;
+	const HISTORY_DEPTH_MIN: i32 = 4;
 	const HISTORY_PRUNE_MOVE_LIMIT: i32 = 5;
 	const HISTORY_THRESHOLD: i32 = 100;
 	const HISTORY_REDUCTION: i32 = 1;

8d56b4ebe15bf4e2adebfaa055bd90aea2fed38b
DkeRee <dkeree1@gmail.com>
2023-02-14 23:18:35 -0800

    lower rfp multiplier

--- src/search/searcher.rs
+++ src/search/searcher.rs
@@ -513,7 +513,7 @@ impl Searcher<'_> {
 
 impl Searcher<'_> {
 	const MAX_DEPTH_RFP: i32 = 6;
-	const MULTIPLIER_RFP: i32 = 100;
+	const MULTIPLIER_RFP: i32 = 80;
 	const NMP_REDUCTION_BASE: i32 = 3;
 	const NMP_XSHIFT: i32 = 2;
 	const NMP_YSTRETCH: i32 = 4;

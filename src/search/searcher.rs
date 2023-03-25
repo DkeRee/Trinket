069d1a1c6787ac99a74253a3a84e94201f567e7e
DkeRee <dkeree1@gmail.com>
2023-03-25 14:18:48 -0700

    hred depth variable

--- src/search/searcher.rs
+++ src/search/searcher.rs
@@ -301,7 +301,7 @@ impl Searcher<'_> {
 				//IF ISNT PV
 				//IF ISNT in check
 				//IF ISNT extended
-				if depth >= Self::HISTORY_DEPTH_MIN && !is_pv && !in_check && moves_searched >= Self::HISTORY_PRUNE_MOVE_LIMIT && !extended {
+				if depth >= ply / 2 + Self::HISTORY_DEPTH_MIN && !is_pv && !in_check && moves_searched >= Self::HISTORY_PRUNE_MOVE_LIMIT && !extended {
 					let history_value = sm.history;
 
 					//History Leaf Reduction

31bb802ee676a9c19eaf380d432dce544c22b9f3
DkeRee <dkeree1@gmail.com>
2023-03-25 13:47:18 -0700

    Try SPP Depth 4

--- src/search/searcher.rs
+++ src/search/searcher.rs
@@ -540,5 +540,5 @@ impl Searcher<'_> {
 	const HISTORY_PRUNE_MOVE_LIMIT: i32 = 5;
 	const HISTORY_THRESHOLD: i32 = 100;
 	const HISTORY_REDUCTION: i32 = 1;
-	const SPP_DEPTH_CAP: i32 = 3;
+	const SPP_DEPTH_CAP: i32 = 4;
 }

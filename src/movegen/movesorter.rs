3b6f30fa24eb97267b936098c8adc2d311577116
DkeRee <dkeree1@gmail.com>
2023-03-27 20:06:50 -0700

    more

--- src/movegen/movesorter.rs
+++ src/movegen/movesorter.rs
@@ -153,7 +153,7 @@ impl MoveSorter {
 	const HASHMOVE_SCORE: i32 = 25000;
 	const WINNING_CAPTURE: i32 = 10000;
 	const QUEEN_PROMO: i32 = 8000;
-	const COUNTERMOVE_SCORE: i32 = 3000;
+	const COUNTERMOVE_SCORE: i32 = 5000;
     const KILLER_MOVE_SCORE: i32 = 2000;
 	const CASTLING_SCORE: i32 = 1000;
    	const KNIGHT_PROMO: i32 = -5000;

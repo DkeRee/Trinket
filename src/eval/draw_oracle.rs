c2df38c2392239f24e7bfb2855d10104ac7b073c
DkeRee <dkeree1@gmail.com>
2023-04-15 22:16:10 -0700

    draw oracle check for varied minor pieces and bishop pair v bishop draws

--- src/eval/draw_oracle.rs
+++ src/eval/draw_oracle.rs
@@ -5,6 +5,8 @@ pub fn oracle_lookup(board: &Board) -> bool {
 	|| (knight_lone_king(board, Color::Black) && bishop_lone_king(board, Color::White))
 	|| varied_minor_pieces(board, Color::White)
 	|| varied_minor_pieces(board, Color::Black)
+	|| bishop_pair_bishop(board, Color::White)
+	|| bishop_pair_bishop(board, Color::Black)
 }
 
 fn knight_lone_king(board: &Board, color: Color) -> bool {
@@ -39,4 +41,16 @@ fn varied_minor_pieces(board: &Board, color: Color) -> bool {
 	let opponent_only_bishop = ((opponent_pieces & board.pieces(Piece::Bishop)) ^ (board.king(!color).bitboard() ^ opponent_pieces)).is_empty();
 
 	me_only_knight && me_only_bishop && me_only_have_knight_bishop && (opponent_only_knight || opponent_only_bishop)
+}
+
+fn bishop_pair_bishop(board: &Board, color: Color) -> bool {
+	let my_pieces = board.colors(color);
+	let opponent_pieces = board.colors(!color);
+
+	let me_only_have_2_bishop = (my_pieces & board.pieces(Piece::Bishop)).len() == 2;
+	let me_only_have_bishops = ((board.king(color).bitboard() ^ my_pieces) ^ (my_pieces & board.pieces(Piece::Bishop))).is_empty();
+
+	let opponent_only_bishop = ((opponent_pieces & board.pieces(Piece::Bishop)) ^ (board.king(!color).bitboard() ^ opponent_pieces)).is_empty();
+
+	me_only_have_2_bishop && me_only_have_bishops && opponent_only_bishop
 }

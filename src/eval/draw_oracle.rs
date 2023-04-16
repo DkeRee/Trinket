b17b55f18f4883695a8a2def34ec07b1c3f1bf3b
DkeRee <dkeree1@gmail.com>
2023-04-16 11:41:46 -0700

    bishop pair draw

--- src/eval/draw_oracle.rs
+++ src/eval/draw_oracle.rs
@@ -3,6 +3,8 @@ use cozy_chess::*;
 pub fn oracle_lookup(board: &Board) -> bool {
 	(knight_lone_king(board, Color::White) && bishop_lone_king(board, Color::Black))
 	|| (knight_lone_king(board, Color::Black) && bishop_lone_king(board, Color::White))
+	|| bishop_pair_bishop(board, Color::White)
+	|| bishop_pair_bishop(board, Color::Black)
 }
 
 fn knight_lone_king(board: &Board, color: Color) -> bool {
@@ -23,4 +25,17 @@ fn bishop_lone_king(board: &Board, color: Color) -> bool {
 	let me_only_have_bishops = ((board.king(color).bitboard() ^ my_pieces) ^ (my_pieces & board.pieces(Piece::Bishop))).is_empty();
 
 	(me_only_bishop && me_only_have_bishops) || me_only_king
+}
+
+fn bishop_pair_bishop(board: &Board, color: Color) -> bool {
+	let my_pieces = board.colors(color);
+	let opponent_pieces = board.colors(!color);
+
+	let my_bishop_number = (my_pieces & board.pieces(Piece::Bishop)).len() as i32;
+	let me_only_have_bishops = ((board.king(color).bitboard() ^ my_pieces) ^ (my_pieces & board.pieces(Piece::Bishop))).is_empty();
+
+	let opponent_bishop_number = (opponent_pieces & board.pieces(Piece::Bishop)).len() as i32;
+	let opponent_only_bishop = ((opponent_pieces & board.pieces(Piece::Bishop)) ^ (board.king(!color).bitboard() ^ opponent_pieces)).is_empty();
+
+	me_only_have_bishops && opponent_only_bishop && my_bishop_number - 1 == opponent_bishop_number
 }

5b9e111e2ce4b66799ba6d98e59323c49d029cf4
DkeRee <dkeree1@gmail.com>
2023-04-17 00:35:22 -0700

    edited oracle to make it more versatile

--- src/eval/draw_oracle.rs
+++ src/eval/draw_oracle.rs
@@ -6,7 +6,7 @@ pub fn oracle_lookup(board: &Board) -> bool {
 
 	((knight_lone_king(board, Color::White) || white_only_king) && (bishop_lone_king(board, Color::Black) || black_only_king))
 	|| ((knight_lone_king(board, Color::Black) || black_only_king) && (bishop_lone_king(board, Color::White) || white_only_king))
-	|| ((knight_lone_king(board, Color::White) || white_only_king) && (knight_lone_king(board, Color::Black) || black_only_king))
+	//|| ((knight_lone_king(board, Color::White) || white_only_king) && (knight_lone_king(board, Color::Black) || black_only_king))
 	/*
 	|| (minor_piece_king(board, Color::White) && knight_lone_king(board, Color::Black))
 	|| (minor_piece_king(board, Color::Black) && knight_lone_king(board, Color::White))
@@ -18,7 +18,6 @@ pub fn oracle_lookup(board: &Board) -> bool {
 fn knight_lone_king(board: &Board, color: Color) -> bool {
 	let my_pieces = board.colors(color);
 
-	let me_only_king = (board.king(color).bitboard() ^ my_pieces).is_empty();
 	let me_two_or_one_knights = (my_pieces & board.pieces(Piece::Knight)).len() == 2 || (my_pieces & board.pieces(Piece::Knight)).len() == 1;
 	let me_only_knights = ((board.king(color).bitboard() ^ my_pieces) ^ (my_pieces & board.pieces(Piece::Knight))).is_empty();
 
@@ -28,7 +27,6 @@ fn knight_lone_king(board: &Board, color: Color) -> bool {
 fn bishop_lone_king(board: &Board, color: Color) -> bool {
 	let my_pieces = board.colors(color);
 
-	let me_only_king = (board.king(color).bitboard() ^ my_pieces).is_empty();
 	let me_only_bishop = (my_pieces & board.pieces(Piece::Bishop)).len() == 1;
 	let me_only_have_bishops = ((board.king(color).bitboard() ^ my_pieces) ^ (my_pieces & board.pieces(Piece::Bishop))).is_empty();
 

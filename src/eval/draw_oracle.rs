3f989e87d269e9430f2cf157bb7aabc593a07160
DkeRee <dkeree1@gmail.com>
2023-04-17 23:52:24 -0700

    dont check pawn knight oracle

--- src/eval/draw_oracle.rs
+++ src/eval/draw_oracle.rs
@@ -4,11 +4,11 @@ pub fn oracle_lookup(board: &Board) -> bool {
 	let white_only_king = (board.king(Color::White).bitboard() ^ board.colors(Color::White)).is_empty();
 	let black_only_king = (board.king(Color::Black).bitboard() ^ board.colors(Color::Black)).is_empty();
 
-	((knight_lone_king(board, Color::White) || white_only_king) && (bishop_lone_king(board, Color::Black) || black_only_king))
-	|| ((knight_lone_king(board, Color::Black) || black_only_king) && (bishop_lone_king(board, Color::White) || white_only_king))
-	|| (knight_lone_king(board, Color::White) && knight_lone_king(board, Color::Black))
-	|| (bishop_pair_lone_king(board, Color::White) && bishop_lone_king(board, Color::Black))
-	|| (bishop_pair_lone_king(board, Color::Black) && bishop_lone_king(board, Color::White))
+	((knight_lone_king(board, Color::White, false) || white_only_king) && (bishop_lone_king(board, Color::Black) || black_only_king))
+	|| ((knight_lone_king(board, Color::Black, false) || black_only_king) && (bishop_lone_king(board, Color::White) || white_only_king))
+	|| (knight_lone_king(board, Color::White, true) && knight_lone_king(board, Color::Black, true))
+	//|| (bishop_pair_lone_king(board, Color::White) && bishop_lone_king(board, Color::Black))
+	//|| (bishop_pair_lone_king(board, Color::Black) && bishop_lone_king(board, Color::White))
 	/*
 	|| (minor_piece_king(board, Color::White) && knight_lone_king(board, Color::Black))
 	|| (minor_piece_king(board, Color::Black) && knight_lone_king(board, Color::White))
@@ -17,8 +17,12 @@ pub fn oracle_lookup(board: &Board) -> bool {
 	*/
 }
 
-fn knight_lone_king(board: &Board, color: Color) -> bool {
-	let my_pieces = board.colors(color);
+fn knight_lone_king(board: &Board, color: Color, no_check_pawn: bool) -> bool {
+	let mut my_pieces = board.colors(color);
+
+	if no_check_pawn {
+		my_pieces ^= (my_pieces & board.pieces(Piece::Pawn));
+	}
 
 	let me_two_or_one_knights = (my_pieces & board.pieces(Piece::Knight)).len() == 2 || (my_pieces & board.pieces(Piece::Knight)).len() == 1;
 	let me_only_knights = ((board.king(color).bitboard() ^ my_pieces) ^ (my_pieces & board.pieces(Piece::Knight))).is_empty();

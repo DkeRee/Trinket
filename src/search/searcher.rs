d61980bc44dd9f42f273c8a27abb9a5952be8592
DkeRee <dkeree1@gmail.com>
2023-04-20 10:59:00 -0700

    extend more if pawn seventh

--- src/search/searcher.rs
+++ src/search/searcher.rs
@@ -356,6 +356,10 @@ impl Searcher<'_> {
 					let passed = (enemy_pawns & block_mask).is_empty() && (my_pawns & get_between_rays(mv.from, Square::new(mv.from.file(), promo_rank))).is_empty();
 					if passed {
 						new_depth += 1;
+
+						if !(mv.from.bitboard() & Rank::Seventh.relative_to(board.side_to_move()).bitboard()).is_empty() {
+							new_depth += 1;
+						}
 					} else {
 						new_depth -= 1;
 					}

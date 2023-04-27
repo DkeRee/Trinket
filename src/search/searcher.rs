a7213e377ee07bf2c5e50d9dfb7aa1e74c40f2ee
DkeRee <dkeree1@gmail.com>
2023-04-27 00:49:44 -0700

    one iter iid

--- src/search/searcher.rs
+++ src/search/searcher.rs
@@ -146,14 +146,9 @@ impl Searcher<'_> {
 				//if sufficient depth
 				//if PV node
 				if depth >= Self::IID_DEPTH_MIN	&& is_pv {
-					let iid_max_depth = depth / 4;
-					let mut iid_depth = 1;
-
-					while iid_depth <= iid_max_depth {
-						let (best_mv, _) = self.search(&abort, board, iid_depth, ply, alpha, beta, past_positions, last_move)?;
-						iid_move = best_mv;
-						iid_depth += 1;
-					}
+					let iid_depth = depth / 4;
+					let (best_mv, _) = self.search(&abort, board, iid_depth, ply, alpha, beta, past_positions, last_move)?;
+					iid_move = best_mv;
 				}
 
 				//Internal Iterative Reduction

d0319b4b96486563303a928afce590b69fe37ae5
DkeRee <dkeree1@gmail.com>
2023-04-29 12:07:03 -0700

    check king corner 2 piece

--- src/eval/draw_oracle.rs
+++ src/eval/draw_oracle.rs
@@ -6,13 +6,10 @@ pub fn oracle_lookup(board: &Board) -> bool {
 
 	((knight_lone_king(board, Color::White) || white_only_king) && (bishop_lone_king(board, Color::Black) || black_only_king))
 	|| ((knight_lone_king(board, Color::Black) || black_only_king) && (bishop_lone_king(board, Color::White) || white_only_king))
-	//|| ((knight_lone_king(board, Color::White) || white_only_king) && (knight_lone_king(board, Color::Black) || black_only_king))
-	/*
 	|| (minor_piece_king(board, Color::White) && knight_lone_king(board, Color::Black))
 	|| (minor_piece_king(board, Color::Black) && knight_lone_king(board, Color::White))
 	|| (minor_piece_king(board, Color::White) && bishop_lone_king(board, Color::Black))
 	|| (minor_piece_king(board, Color::Black) && bishop_lone_king(board, Color::White))
-	*/
 }
 
 fn knight_lone_king(board: &Board, color: Color) -> bool {
@@ -21,7 +18,7 @@ fn knight_lone_king(board: &Board, color: Color) -> bool {
 	let me_two_or_one_knights = (my_pieces & board.pieces(Piece::Knight)).len() == 2 || (my_pieces & board.pieces(Piece::Knight)).len() == 1;
 	let me_only_knights = ((board.king(color).bitboard() ^ my_pieces) ^ (my_pieces & board.pieces(Piece::Knight))).is_empty();
 
-	me_two_or_one_knights && me_only_knights && (board.pieces(Piece::King) & BitBoard::EDGES).is_empty()
+	me_two_or_one_knights && me_only_knights
 }
 
 fn bishop_lone_king(board: &Board, color: Color) -> bool {
@@ -41,5 +38,5 @@ fn minor_piece_king(board: &Board, color: Color) -> bool {
 	let me_only_one_bishop = (my_pieces & board.pieces(Piece::Bishop)).len() == 1;	
     let me_only_minor = ((my_pieces & (board.pieces(Piece::Bishop) | board.pieces(Piece::Knight))) ^ (board.king(color).bitboard() ^ my_pieces)).is_empty();
 
-	me_only_one_knight && me_only_one_bishop && me_only_minor
+	me_only_one_knight && me_only_one_bishop && me_only_minor && (board.pieces(Piece::King) & BitBoard::CORNERS).is_empty()
 }

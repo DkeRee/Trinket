586f4c8704c1ceea26ae489cb48e3fdb8383dbc4
DkeRee <dkeree1@gmail.com>
2023-04-30 17:58:47 -0700

    dont extend underpromo

--- src/search/searcher.rs
+++ src/search/searcher.rs
@@ -365,7 +365,13 @@ impl Searcher<'_> {
 					//check to see if these three BB files contain enemy pawns in them && and if this is not a pawn island
 					let passed = (enemy_pawns & block_mask).is_empty() && (my_pawns & get_between_rays(mv.from, Square::new(mv.from.file(), promo_rank))).is_empty();
 					if passed {
-						new_depth += 1;
+						if mv.promotion.is_some() {
+							if mv.promotion.unwrap() == Piece::Queen {
+								new_depth += 1;
+							}
+						} else {
+							new_depth += 1;
+						}
 					} else {
 						new_depth -= 1;
 					}

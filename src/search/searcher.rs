3e5131f3fa18141db773396954ba475d6dab2f9a
DkeRee <dkeree1@gmail.com>
2023-05-26 19:53:06 -0700

    try more improving lmp

--- src/search/searcher.rs
+++ src/search/searcher.rs
@@ -295,7 +295,7 @@ impl Searcher<'_> {
 				//IF alpha is NOT a losing mate
 				//IF IS late move
 				//IF is NOT a check
-				if !is_pv && depth <= Self::LMP_DEPTH_MAX && sm.movetype == MoveType::Quiet && alpha > -Score::CHECKMATE_DEFINITE && moves_searched > (Self::LMP_MULTIPLIER * depth) - (!improving as i32 * 3) && !in_check {
+				if !is_pv && depth <= Self::LMP_DEPTH_MAX && sm.movetype == MoveType::Quiet && alpha > -Score::CHECKMATE_DEFINITE && moves_searched > (Self::LMP_MULTIPLIER * depth) - (!improving as i32 * 6) && !in_check {
 					past_positions.pop();
 					break;
 				}

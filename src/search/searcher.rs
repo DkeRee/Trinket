9d421658db834836a9c110fdd64fcec540dc226d
DkeRee <dkeree1@gmail.com>
2023-07-04 12:48:20 -0700

    side pawn push extension 3

--- src/search/searcher.rs
+++ src/search/searcher.rs
@@ -373,10 +373,12 @@ impl Searcher<'_> {
 					}
 				}
 
-				//Side Pawn Push Extension
+				//Side Pawn Push Reduction
 				let side_pawns = my_pawns & Rank::Second.relative_to(board.side_to_move()).bitboard() & (File::A.bitboard() | File::H.bitboard());
-				if is_pv && !(mv.from.bitboard() & side_pawns).is_empty() && !(mv.to.bitboard() & get_pawn_quiets(mv.from, board.side_to_move(), BitBoard::EMPTY)).is_empty() && improving {
-					new_depth += 1;
+				if is_pv && !(mv.from.bitboard() & side_pawns).is_empty() && !(mv.to.bitboard() & get_pawn_quiets(mv.from, board.side_to_move(), BitBoard::EMPTY)).is_empty() {
+					if (board.king(board.side_to_move()).bitboard() & get_pawn_attacks(mv.from, !board.side_to_move())).is_empty() {
+						new_depth += 1;
+					}
 				}
 
 				if in_check || sm.is_killer || sm.is_countermove {

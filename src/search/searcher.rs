e7279c4bbe055256f067cdc5b3cc66e3ebd65153
DkeRee <dkeree1@gmail.com>
2024-01-07 09:12:12 -0800

    try this

--- src/search/searcher.rs
+++ src/search/searcher.rs
@@ -349,7 +349,7 @@ impl Searcher<'_> {
 				let ranks = Rank::Seventh.relative_to(board.side_to_move()).bitboard() | Rank::Sixth.relative_to(board.side_to_move()).bitboard();
 				let pawn_on_ranks = my_pawns & ranks;
 				let exists = !(mv.from.bitboard() & pawn_on_ranks).is_empty();
-				if exists {
+				if exists && is_pv {
 					//pawn exists, check if it's a passer
 					let promo_rank = Rank::Eighth.relative_to(board.side_to_move());
 					let mut pawn_goal = Square::new(mv.from.file(), promo_rank);
@@ -368,9 +368,7 @@ impl Searcher<'_> {
 					//check to see if these three BB files contain enemy pawns in them && and if this is not a pawn island
 					let passed = (enemy_pawns & block_mask).is_empty() && (my_pawns & get_between_rays(mv.from, Square::new(mv.from.file(), promo_rank))).is_empty();
 					if passed {
-						if is_pv {
-							reduction -= 1;
-						}
+						reduction -= 1;
 					} else {
 						reduction += 1;
 					}

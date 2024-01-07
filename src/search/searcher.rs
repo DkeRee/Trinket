cd3ca5641a9a45379bbf6cdb5f10b7f4d7cc508e
DkeRee <dkeree1@gmail.com>
2024-01-06 20:55:36 -0800

    try this

--- src/search/searcher.rs
+++ src/search/searcher.rs
@@ -367,7 +367,9 @@ impl Searcher<'_> {
 
 					//check to see if these three BB files contain enemy pawns in them && and if this is not a pawn island
 					let passed = (enemy_pawns & block_mask).is_empty() && (my_pawns & get_between_rays(mv.from, Square::new(mv.from.file(), promo_rank))).is_empty();
-					if !passed {
+					if passed {
+						reduction -= 1;
+					} else {
 						reduction += 1;
 					}
 				}

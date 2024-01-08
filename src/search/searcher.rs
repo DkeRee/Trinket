3d7395b6bf13aa36279d670107e05b274f7d1a61
DkeRee <dkeree1@gmail.com>
2024-01-08 10:18:19 -0800

    rework history reduction 3

--- src/search/searcher.rs
+++ src/search/searcher.rs
@@ -311,7 +311,7 @@ impl Searcher<'_> {
 				let mut reduction = 0;
 
 				//History Leaf Reduction
-				reduction -= sm.history / 600;
+				reduction -= sm.history / 1000;
 
 				//LMR can be applied
 				//IF depth is above sufficient depth

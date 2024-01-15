bd5047b1fbca70304e00fa9285b4327f7af5e174
DkeRee <dkeree1@gmail.com>
2024-01-14 18:41:45 -0800

    raise to 6 killers

--- src/movegen/movesorter.rs
+++ src/movegen/movesorter.rs
@@ -9,7 +9,7 @@ pub enum MoveType {
 }
 
 pub struct MoveSorter {
-	killer_table: [[[Option<Move>; 2]; 100]; 2],
+	killer_table: [[[Option<Move>; 6]; 100]; 2],
 	history_table: [[i32; 64]; 64],
 	countermove_table: [[Option<Move>; 64]; 64],
 	see: See
@@ -18,7 +18,7 @@ pub struct MoveSorter {
 impl MoveSorter {
 	pub fn new () -> MoveSorter {
 		MoveSorter {
-			killer_table: [[[None; 2]; 100]; 2],
+			killer_table: [[[None; 6]; 100]; 2],
 			history_table: [[0; 64]; 64],
 			countermove_table: [[None; 64]; 64],
 			see: See::new()

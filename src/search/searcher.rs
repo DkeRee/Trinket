570d3b1adbd11bc0703a1016d58907b6545ab6c9
DkeRee <dkeree1@gmail.com>
2024-01-16 21:11:43 -0800

    try this

--- src/search/searcher.rs
+++ src/search/searcher.rs
@@ -416,14 +416,11 @@ impl Searcher<'_> {
 						self.tt.insert(best_move, eval.score, board.hash(), ply, depth, NodeKind::Exact);
 					}
 				} else {
-					//SPP
-					//IF is NOT PV
-					//IF is of reasonable depth
-					//IF move does NOT give check
-					//IF is quiet move
-					do_spp = !is_pv && depth <= Self::SPP_DEPTH_CAP && !move_is_check && sm.movetype == MoveType::Quiet;
 					self.tt.insert(best_move, eval.score, board.hash(), ply, depth, NodeKind::UpperBound);
 				}
+			} else {
+				//SPP
+				do_spp = !is_pv && depth <= Self::SPP_DEPTH_CAP && !move_is_check && sm.movetype == MoveType::Quiet;
 			}
 
 			sm.decay_history(&mut self.movegen.sorter, depth);
@@ -515,7 +512,7 @@ impl Searcher<'_> {
 		for sm in move_list {
 
 			//prune losing captures found through SEE swap algorithm
-			if sm.importance < 0 {
+			if eval.score + 100 <= alpha {
 				break;
 			}
 

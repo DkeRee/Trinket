fc650fc24fb4518a13108ccb5cde927ef2b3b81a
DkeRee <dkeree1@gmail.com>
2024-01-31 19:16:21 -0800

    no checkmul history

--- src/movegen/movesorter.rs
+++ src/movegen/movesorter.rs
@@ -86,11 +86,9 @@ impl MoveSorter {
 
 	pub fn add_history(&mut self, mv: Move, depth: i32) {
 		let history = self.history_table[mv.from as usize][mv.to as usize];
-		let change = depth * depth + 10;
+		let change = depth * depth;
 
-		if !change.checked_mul(history).is_none() {
-			self.history_table[mv.from as usize][mv.to as usize] += change - change * history / Self::HISTORY_MAX; //add quiet score into history table based on from and to squares
-		}
+		self.history_table[mv.from as usize][mv.to as usize] += change - change * history / Self::HISTORY_MAX;
 	}
 
 	pub fn add_countermove(&mut self, mv: Move, last_move: Move) {
@@ -101,9 +99,7 @@ impl MoveSorter {
 		let history = self.history_table[mv.from as usize][mv.to as usize];
 		let change = depth * depth;
 
-		if !change.checked_mul(history).is_none() {
-			self.history_table[mv.from as usize][mv.to as usize] -= change + change * history / Self::HISTORY_MAX; //decay quiet score into history table based on from and to squares
-		}
+		self.history_table[mv.from as usize][mv.to as usize] -= change + change * history / Self::HISTORY_MAX;
 	}
 
 	fn is_killer(&self, mv: Move, board: &Board, ply: i32) -> bool {

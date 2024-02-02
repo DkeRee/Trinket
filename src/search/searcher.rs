92419d74a0b8fb2d6bb684920bc7ac3ef643509a
DkeRee <dkeree1@gmail.com>
2024-02-02 14:46:43 -0800

    lmp prune losing mates

--- src/search/searcher.rs
+++ src/search/searcher.rs
@@ -309,10 +309,9 @@ impl Searcher<'_> {
 				//IF isn't PV
 				//IF low depth
 				//IF move is quiet
-				//IF alpha is NOT a losing mate
 				//IF IS late move
 				//IF is NOT a check
-				if !is_pv && depth <= Self::LMP_DEPTH_MAX && sm.movetype == MoveType::Quiet && alpha > -Score::CHECKMATE_DEFINITE && moves_searched > (Self::LMP_MULTIPLIER * depth) - (!improving as i32 * 3) && !in_check {
+				if !is_pv && depth <= Self::LMP_DEPTH_MAX && sm.movetype == MoveType::Quiet && moves_searched > (Self::LMP_MULTIPLIER * depth) - (!improving as i32 * 3) && !in_check {
 					past_positions.pop();
 					break;
 				}

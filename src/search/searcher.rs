99a0c269133716e9f2a1e459926c8082ebc06c89
Daniel Ke <dkeree1@gmail.com>
2024-08-10 14:36:47 -0700

    simplify iir

--- src/search/searcher.rs
+++ src/search/searcher.rs
@@ -152,8 +152,8 @@ impl Searcher<'_> {
 				//Internal Iterative Reduction
 				//IF sufficient depth
 				//There is NO Hash Move
-				if depth >= ply / 4 + 2 {
-					depth -= depth / 10 + 1;
+				if depth >= 3 {
+					depth -= 1;
 				}
 
 				(None, iid_move)

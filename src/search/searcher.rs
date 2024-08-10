f8ac89d8bc685b0123dc8db65037f54dcbc4bf54
Daniel Ke <dkeree1@gmail.com>
2024-08-10 14:54:36 -0700

    agressive underpromo reduction

--- src/search/searcher.rs
+++ src/search/searcher.rs
@@ -338,7 +338,7 @@ impl Searcher<'_> {
 				//Underpromo Reduction
 				if !mv.promotion.is_none() {
 					if mv.promotion.unwrap() != Piece::Queen && depth >= Self::UNDERPROMO_REDUC_DEPTH {
-						reduction += 1;
+						reduction += 3;
 					}
 				}
 

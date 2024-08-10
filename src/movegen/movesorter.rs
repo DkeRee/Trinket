9835cdd4748978c27e63ea5bc5568cb1e5a6a7db
Daniel Ke <dkeree1@gmail.com>
2024-08-10 15:03:55 -0700

    make killer moves and countermoves less important

--- src/movegen/movesorter.rs
+++ src/movegen/movesorter.rs
@@ -140,8 +140,8 @@ impl MoveSorter {
 	const HASHMOVE_SCORE: i32 = 25000;
 	const WINNING_CAPTURE: i32 = 10000;
 	const QUEEN_PROMO: i32 = 8000;
-    const KILLER_MOVE_SCORE: i32 = 2000;
-	const COUNTERMOVE_SCORE: i32 = 1000;
+    const KILLER_MOVE_SCORE: i32 = 500;
+	const COUNTERMOVE_SCORE: i32 = 500;
    	const KNIGHT_PROMO: i32 = -5000;
 	const BISHOP_PROMO: i32 = -6000;
 	const ROOK_PROMO: i32 = -7000;

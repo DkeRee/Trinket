3600cf4c541cac799227468791f9a2429164b5a3
Daniel Ke <dkeree1@gmail.com>
2024-08-10 15:17:41 -0700

    make pruning losing captures more strict

--- src/search/searcher.rs
+++ src/search/searcher.rs
@@ -522,7 +522,7 @@ impl Searcher<'_> {
 		for sm in move_list {
 
 			//prune losing captures found through SEE swap algorithm
-			if sm.importance < 0 {
+			if sm.importance < -1000 {
 				break;
 			}
 

ad7f6abc8998501069ecaedbbb1abbb305d65f0d
DkeRee <68255841+DkeRee@users.noreply.github.com>
2026-04-23 10:31:16 -0400

    bench: 4494929

--- src/search/searcher.rs
+++ src/search/searcher.rs
@@ -431,6 +431,13 @@ impl Searcher<'_> {
 
 					self.tt.insert(best_move, eval.score, board.hash(), ply, depth, NodeKind::UpperBound);
 				}
+			} else {
+				do_spp = !is_pv 
+				&& depth <= Self::SPP_DEPTH_CAP 
+				&& !move_is_check 
+				&& !sm.is_killer
+				&& !sm.is_countermove
+				&& sm.movetype == MoveType::Quiet;		
 			}
 
 			sm.decay_history(&mut self.movegen.sorter, depth);

21fc8f22513438b93449a0f90849677d88cedd9d
DkeRee <68255841+DkeRee@users.noreply.github.com>
2026-04-27 13:57:10 -0400

    bench: 4877133

--- src/eval/draw_oracle.rs
+++ src/eval/draw_oracle.rs
@@ -3,43 +3,81 @@ use cozy_chess::*;
 pub fn oracle_lookup(board: &Board) -> bool {
 	let white_only_king = (board.king(Color::White).bitboard() ^ board.colors(Color::White)).is_empty();
 	let black_only_king = (board.king(Color::Black).bitboard() ^ board.colors(Color::Black)).is_empty();
+	let mut draw = false;
 
-	((knight_lone_king(board, Color::White) || white_only_king) && (bishop_lone_king(board, Color::Black) || black_only_king))
-	|| ((knight_lone_king(board, Color::Black) || black_only_king) && (bishop_lone_king(board, Color::White) || white_only_king))
-	//|| ((knight_lone_king(board, Color::White) || white_only_king) && (knight_lone_king(board, Color::Black) || black_only_king))
-	/*
-	|| (minor_piece_king(board, Color::White) && knight_lone_king(board, Color::Black))
-	|| (minor_piece_king(board, Color::Black) && knight_lone_king(board, Color::White))
-	|| (minor_piece_king(board, Color::White) && bishop_lone_king(board, Color::Black))
-	|| (minor_piece_king(board, Color::Black) && bishop_lone_king(board, Color::White))
-	*/
+	//one knight one bishop + one bishop one knight
+	draw = one_knight_king(board, Color::White) && one_bishop_king(board, Color::Black);
+	draw = one_knight_king(board, Color::Black) && one_bishop_king(board, Color::White);
+
+	//one bishop + one bishop
+	draw = one_bishop_king(board, Color::White) && one_bishop_king(board, Color::Black);
+
+	//one knight + one knight
+	draw = one_knight_king(board, Color::White) && one_knight_king(board, Color::Black);
+
+	//two knights + lone king
+	draw = two_knight_king(board, Color::White) && black_only_king;
+	draw = two_knight_king(board, Color::Black) && white_only_king;
+
+	//one rook + one rook
+	draw = one_rook_king(board, Color::White) && one_rook_king(board, Color::Black);
+
+	//one queen + one queen
+	draw = one_queen_king(board, Color::White) && one_queen_king(board, Color::Black);
+
+	draw
 }
 
-fn knight_lone_king(board: &Board, color: Color) -> bool {
+fn one_knight_king(board: &Board, color: Color) -> bool {
 	let my_pieces = board.colors(color);
 
-	let me_two_or_one_knights = (my_pieces & board.pieces(Piece::Knight)).len() == 2 || (my_pieces & board.pieces(Piece::Knight)).len() == 1;
+	let me_one_knight = (my_pieces & board.pieces(Piece::Knight)).len() == 2 || (my_pieces & board.pieces(Piece::Knight)).len() == 1;
 	let me_only_knights = ((board.king(color).bitboard() ^ my_pieces) ^ (my_pieces & board.pieces(Piece::Knight))).is_empty();
 
-	me_two_or_one_knights && me_only_knights
+	me_one_knight && me_only_knights
 }
 
-fn bishop_lone_king(board: &Board, color: Color) -> bool {
+fn one_bishop_king(board: &Board, color: Color) -> bool {
 	let my_pieces = board.colors(color);
 
-	let me_only_bishop = (my_pieces & board.pieces(Piece::Bishop)).len() == 1;
+	let me_one_bishop = (my_pieces & board.pieces(Piece::Bishop)).len() == 1;
 	let me_only_have_bishops = ((board.king(color).bitboard() ^ my_pieces) ^ (my_pieces & board.pieces(Piece::Bishop))).is_empty();
 
-	me_only_bishop && me_only_have_bishops
+	me_one_bishop && me_only_have_bishops
 }
 
-fn minor_piece_king(board: &Board, color: Color) -> bool {
+fn two_knight_king(board: &Board, color: Color) -> bool {
 	let my_pieces = board.colors(color);
-	let opponent_pieces = board.colors(!color);
 
-	let me_only_one_knight = (my_pieces & board.pieces(Piece::Knight)).len() == 1;
-	let me_only_one_bishop = (my_pieces & board.pieces(Piece::Bishop)).len() == 1;	
-    let me_only_minor = ((my_pieces & (board.pieces(Piece::Bishop) | board.pieces(Piece::Knight))) ^ (board.king(color).bitboard() ^ my_pieces)).is_empty();
+	let me_two_knights = (my_pieces & board.pieces(Piece::Knight)).len() == 2;
+	let me_only_knights = ((board.king(color).bitboard() ^ my_pieces) ^ (my_pieces & board.pieces(Piece::Knight))).is_empty();
 
-	me_only_one_knight && me_only_one_bishop && me_only_minor
+	me_two_knights && me_only_knights
+}
+
+fn two_bishop_king(board: &Board, color: Color) -> bool {
+	let my_pieces = board.colors(color);
+
+	let me_two_bishops = (my_pieces & board.pieces(Piece::Bishop)).len() == 2;
+	let me_only_have_bishops = ((board.king(color).bitboard() ^ my_pieces) ^ (my_pieces & board.pieces(Piece::Bishop))).is_empty();
+
+	me_two_bishops && me_only_have_bishops
+}
+
+fn one_rook_king(board: &Board, color: Color) -> bool {
+	let my_pieces = board.colors(color);
+
+	let me_one_rook = (my_pieces & board.pieces(Piece::Rook)).len() == 1;
+	let me_only_have_rooks = ((board.king(color).bitboard() ^ my_pieces) ^ (my_pieces & board.pieces(Piece::Rook))).is_empty();
+
+	me_one_rook && me_only_have_rooks
+}
+
+fn one_queen_king(board: &Board, color: Color) -> bool {
+	let my_pieces = board.colors(color);
+
+	let me_one_queen = (my_pieces & board.pieces(Piece::Queen)).len() == 1;
+	let me_only_have_queens = ((board.king(color).bitboard() ^ my_pieces) ^ (my_pieces & board.pieces(Piece::Queen))).is_empty();
+
+	me_one_queen && me_only_have_queens
 }

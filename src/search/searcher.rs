20037763bb2d601e791bc8062ff4452fbfa55891
DkeRee <68255841+DkeRee@users.noreply.github.com>
2026-05-05 00:09:03 -0400

    bench: 4882879

--- src/search/searcher.rs
+++ src/search/searcher.rs
@@ -432,7 +432,7 @@ impl Searcher<'_> {
 			}
 		}
 
-		if !in_check
+		if in_check
 		&& (best_move.is_none() || matches!(best_move_type, Some(MoveType::Quiet))) {
 			self.movegen.sorter.add_pawn_corrhist(board, depth, eval.score, static_eval);
 		}

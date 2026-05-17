0aebe68f2cadb4093e508ccae61278ff4f9d86f6
DkeRee <68255841+DkeRee@users.noreply.github.com>
2026-05-17 13:19:37 -0700

    bench: 5139750

--- src/movegen/movegen.rs
+++ src/movegen/movegen.rs
@@ -29,9 +29,9 @@ impl SortedMove {
 		}
 	}
 
-	pub fn insert_history(&mut self, move_sorter: &mut MoveSorter, depth: i32) {
+	pub fn insert_history(&mut self, move_sorter: &mut MoveSorter, depth: i32, board: &Board) {
 		if self.movetype == MoveType::Quiet {
-			move_sorter.add_history(self.mv, depth);
+			move_sorter.add_history(self.mv, depth, board);
 		}
 	}
 
@@ -41,9 +41,9 @@ impl SortedMove {
 		}
 	}
 
-	pub fn decay_history(&mut self, move_sorter: &mut MoveSorter, depth: i32) {
+	pub fn decay_history(&mut self, move_sorter: &mut MoveSorter, depth: i32, board: &Board) {
 		if self.movetype == MoveType::Quiet {
-			move_sorter.decay_history(self.mv, depth);
+			move_sorter.decay_history(self.mv, depth, board);
 		}
 	}
 }

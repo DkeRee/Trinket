0a2882e95bbf805218d1438d2a3d3bf7a7febb7f
DkeRee <68255841+DkeRee@users.noreply.github.com>
2026-05-17 23:30:20 -0700

    bench: 6064301

--- src/search/searcher.rs
+++ src/search/searcher.rs
@@ -600,13 +600,12 @@ impl Searcher<'_> {
 
 		self.shared_info.tt.insert(best_move, eval.score, boardwrapper.board.hash(), ply, depth, tt_nodetype);
 
-		if best_move_type.is_some() {
-			if best_move_type.unwrap() == MoveType::Quiet
-			&& ( (tt_nodetype == NodeKind::UpperBound && eval.score < static_eval) || (tt_nodetype == NodeKind::LowerBound && eval.score > static_eval) ) {
-				self.movegen.sorter.add_pawn_corrhist(boardwrapper, depth, eval.score, static_eval);
-				self.movegen.sorter.add_non_pawn_corrhist(boardwrapper, depth, eval.score, static_eval);
-				self.movegen.sorter.add_material_corrhist(boardwrapper, depth, eval.score, static_eval);
-			}
+		if !in_check
+		&& best_move_type.unwrap() == MoveType::Quiet
+		&& ( (tt_nodetype == NodeKind::Exact) || (tt_nodetype == NodeKind::UpperBound && eval.score < static_eval) || (tt_nodetype == NodeKind::LowerBound && eval.score > static_eval) ) {
+			self.movegen.sorter.add_pawn_corrhist(boardwrapper, depth, eval.score, static_eval);
+			self.movegen.sorter.add_non_pawn_corrhist(boardwrapper, depth, eval.score, static_eval);
+			self.movegen.sorter.add_material_corrhist(boardwrapper, depth, eval.score, static_eval);
 		}
 
 		return Some((best_move, eval));

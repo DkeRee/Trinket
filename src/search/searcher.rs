d1873e14174ad835571ee090c6e672a1cb10c574
DkeRee <68255841+DkeRee@users.noreply.github.com>
2026-05-20 12:02:36 -0700

    bench: 5028339

--- src/search/searcher.rs
+++ src/search/searcher.rs
@@ -473,13 +473,8 @@ impl Searcher<'_> {
 				//History Leaf Reduction
 				reduction -= sm.history / 1500;
 
-				//LMR can be applied
-				//IF depth is above sufficient depth
-				//IF the first X searched are searched
-				if moves_searched >= 2 
-				&& (!is_pv || sm.movetype == MoveType::Quiet || !move_is_check) {
-					reduction += self.get_lmr_reduction_amount(depth, moves_searched);
-				}
+				//LMR
+				reduction += self.get_lmr_reduction_amount(depth, moves_searched);
 
 				//Reduce less if PV node
 				reduction -= is_pv as i32;

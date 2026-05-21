976418bd564a77cfe0e3b1f8d5731873ad96d05f
DkeRee <68255841+DkeRee@users.noreply.github.com>
2026-05-20 21:54:24 -0700

    bench: 5605877

--- src/eval/evaluator.rs
+++ src/eval/evaluator.rs
@@ -359,7 +359,7 @@ impl Evaluator<'_> {
 	const ROOK_PHASE: i32 = 2;
 	const QUEEN_PHASE: i32 = 4;
 	const TOTAL_PIECE_PHASE: i32 = 24;
-	const ORACLE_SCALE: i32 = 100;
+	const ORACLE_SCALE: i32 = 16;
 }
 
 pub fn evaluate(board: &Board) -> i32 {

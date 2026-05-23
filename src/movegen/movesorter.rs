fbe0f9fc74e987dd45ece42672fd7fb54e4f2e16
DkeRee <68255841+DkeRee@users.noreply.github.com>
2026-05-22 18:19:45 -0700

    bench: 5919422

--- src/movegen/movesorter.rs
+++ src/movegen/movesorter.rs
@@ -224,7 +224,7 @@ impl MoveSorter {
 
 	pub fn read_major_corrhist(&mut self, boardwrapper: &BoardWrapper) -> f32 {
 		let pawn_hist = self.major_corrhist[boardwrapper.board.side_to_move() as usize][(boardwrapper.major_hash % Self::CORRHIST_SIZE as u64) as usize];
-		pawn_hist / 210.0
+		pawn_hist / 140.0
 	}
 
 	fn is_countermove(&self, mv: Move, last_move: Option<Move>) -> bool {

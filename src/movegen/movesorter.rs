5dcf00d90f3f36bb098559550d108aab8622560a
DkeRee <68255841+DkeRee@users.noreply.github.com>
2026-05-23 16:34:55 -0700

    bench: 5531516

--- src/movegen/movesorter.rs
+++ src/movegen/movesorter.rs
@@ -111,9 +111,9 @@ impl MoveSorter {
 						};
 					} else {
 						let history = self.get_history(mv_info.mv, board);
-						let conthist = self.get_conthist(mv_info.mv, ply, board);
+						let conthist = self.get_conthist(mv_info.mv, ply, board) * 3 / 4;
 
-						increment = history + conthist / 4;
+						increment = history + conthist;
 						mv_info.history = history;
 					}
 				}

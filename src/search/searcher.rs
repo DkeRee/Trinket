17f81d9f7501ce37c981eb631645d4df5b85f85e
DkeRee <68255841+DkeRee@users.noreply.github.com>
2026-05-24 12:47:43 -0700

    bench: 5347794

--- src/search/searcher.rs
+++ src/search/searcher.rs
@@ -572,12 +572,6 @@ impl Searcher<'_> {
 						sm.insert_history(&mut self.movegen.sorter, depth, &boardwrapper.board);
 						sm.insert_countermove(&mut self.movegen.sorter, last_move);
 
-						if legal_index > 0 {
-							for i in 0..(legal_index - 1) {
-								legal_moves[i as usize].decay_conthist(&mut self.movegen.sorter, depth, ply, &boardwrapper.board);
-							}
-						}
-
 						break;
 					} else {
 						tt_nodetype = NodeKind::Exact;
@@ -595,6 +589,7 @@ impl Searcher<'_> {
 			}
 
 			sm.decay_history(&mut self.movegen.sorter, depth, &boardwrapper.board);
+			sm.decay_conthist(&mut self.movegen.sorter, depth, ply, &boardwrapper.board);
 
 			if do_spp {
 				break;
@@ -606,7 +601,6 @@ impl Searcher<'_> {
 			if staged_movegen && legal_index >= legal_moves.len() {
 				staged_movegen = false;
 				legal_index = 0; 
-				legal_moves[0].decay_conthist(&mut self.movegen.sorter, depth, ply, &boardwrapper.board);
 				legal_moves = self.movegen.move_gen(&boardwrapper.board, Some(mv), ply, true, last_move);
 			}
 		}

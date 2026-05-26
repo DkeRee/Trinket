023bde644b5fdac0ae33cd8984595faf69fe22a0
DkeRee <68255841+DkeRee@users.noreply.github.com>
2026-05-25 20:37:46 -0700

    bench: 4565724

--- src/search/searcher.rs
+++ src/search/searcher.rs
@@ -320,11 +320,13 @@ impl Searcher<'_> {
 			let pawn_corrhist = self.movegen.sorter.read_pawn_corrhist(boardwrapper);
 			let non_pawn_corrhist = self.movegen.sorter.read_non_pawn_corrhist(boardwrapper);
 			let material_corrhist = self.movegen.sorter.read_material_corrhist(boardwrapper);
+			let cont_corrhist = self.movegen.sorter.read_cont_corrhist(ply);
 
 			(base_eval 
 				+ pawn_corrhist 
 				+ non_pawn_corrhist
-				+ material_corrhist) as i32
+				+ material_corrhist
+				+ cont_corrhist) as i32
 		};
 
 		self.evals[ply as usize] = static_eval;
@@ -619,6 +621,7 @@ impl Searcher<'_> {
 			&& ( (tt_nodetype == NodeKind::UpperBound && eval.score < static_eval) || (tt_nodetype == NodeKind::LowerBound && eval.score > static_eval) ) {
 				self.movegen.sorter.add_pawn_corrhist(boardwrapper, depth, eval.score, static_eval);
 				self.movegen.sorter.add_non_pawn_corrhist(boardwrapper, depth, eval.score, static_eval);
+				self.movegen.sorter.add_cont_corrhist(depth, ply, eval.score, static_eval);
 				self.movegen.sorter.add_material_corrhist(boardwrapper, depth, eval.score, static_eval);
 			}
 		}
@@ -644,8 +647,9 @@ impl Searcher<'_> {
 		let pawn_corrhist = self.movegen.sorter.read_pawn_corrhist(boardwrapper);
 		let non_pawn_corrhist = self.movegen.sorter.read_non_pawn_corrhist(boardwrapper);
 		let material_corrhist = self.movegen.sorter.read_material_corrhist(boardwrapper);
+		let cont_corrhist = self.movegen.sorter.read_cont_corrhist(ply);
 		let stand_pat = Eval::new((
-			base_eval + pawn_corrhist + non_pawn_corrhist + material_corrhist
+			base_eval + pawn_corrhist + non_pawn_corrhist + material_corrhist + cont_corrhist
 		) as i32, false);
 
 		//beta cutoff

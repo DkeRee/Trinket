4ab2ed7dc6302de3c3082976cd872500cf670281
DkeRee <68255841+DkeRee@users.noreply.github.com>
2026-05-27 16:10:52 -0700

    bench: 4500575

--- src/search/searcher.rs
+++ src/search/searcher.rs
@@ -313,7 +313,7 @@ impl Searcher<'_> {
 		};
 
 		//static eval for tuning methods
-		let static_eval = if tt_hit.as_ref().is_some() {
+		let static_eval = if tt_hit.as_ref().is_some() && !in_check {
 			tt_hit.as_ref().unwrap().eval
 		} else {
 			let base_eval = evaluate(&boardwrapper.board) as f32;

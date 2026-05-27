a75d91e3b3f83ec1f9b54ff489ab7714deb578f0
DkeRee <68255841+DkeRee@users.noreply.github.com>
2026-05-26 22:06:19 -0700

    bench: 4710674

--- src/search/searcher.rs
+++ src/search/searcher.rs
@@ -377,6 +377,18 @@ impl Searcher<'_> {
 			}
 		}
 
+		//Razoring
+		if !is_pv
+		&& !in_check
+		&& depth < 6
+		&& static_eval <= alpha - 75 * depth - 185 {
+			let (_, v) = self.search(&abort, boardwrapper, 0, ply, alpha, beta, past_positions, last_move)?;
+
+			if v.score <= alpha {
+				return Some((None, Eval::new(v.score, false)));
+			}
+		}
+
 		let mut best_move = None;
 		let mut best_move_type = None;
 		let mut eval = Eval::new(i32::MIN, false);

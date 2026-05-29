f2beede0faea22dca9118779aed05fdc853d5f71
DkeRee <68255841+DkeRee@users.noreply.github.com>
2026-05-29 12:00:21 -0700

    bench: 4091130

--- src/search/searcher.rs
+++ src/search/searcher.rs
@@ -245,13 +245,13 @@ impl Searcher<'_> {
 			GameStatus::Ongoing => {}
 		}
 
-		if depth <= 0 {
-			return self.qsearch(&abort, boardwrapper, alpha, beta, ply); //proceed with qSearch to avoid horizon effect
-		}
-
 		//check for three move repetition
 		if self.is_repetition(&boardwrapper.board, past_positions) && ply > 0 {
 			return Some((None, Eval::new(Score::DRAW, false)));
+		}
+
+		if depth <= 0 {
+			return self.qsearch(&abort, boardwrapper, alpha, beta, ply); //proceed with qSearch to avoid horizon effect
 		}
 
 		let mut legal_moves: Vec<SortedMove> = Vec::with_capacity(64);

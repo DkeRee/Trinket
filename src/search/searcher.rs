7fef1b2f7b003da0b14fba0a41fdbcd876102f22
DkeRee <68255841+DkeRee@users.noreply.github.com>
2026-06-06 08:41:58 -0700

    bench: 4492810

--- src/search/searcher.rs
+++ src/search/searcher.rs
@@ -780,21 +780,24 @@ impl Searcher<'_> {
 		let mut tt_nodetype = NodeKind::UpperBound;
 
 		for mut sm in move_list {
-
-			//prune losing captures found through SEE swap algorithm
-			if sm.importance < 0 {
-				break;
-			}
-
-			//Delta Pruning
-			let captured_value = See::piece_pts(boardwrapper.board.piece_on(sm.mv.to).unwrap());
-			if !in_check && eval.score + captured_value + 200 <= alpha {
-				continue;
-			}
-
 			let mv = sm.mv;
 			let mut board_wrapper_cache = boardwrapper.clone();
 			board_wrapper_cache.play_unchecked(&mut sm);
+
+			let move_is_check = !board_wrapper_cache.board.checkers().is_empty();
+
+			//prune losing captures found through SEE swap algorithm
+			if !move_is_check {
+				if sm.importance < 0 {
+					break;
+				}
+
+				//Delta Pruning
+				let captured_value = See::piece_pts(boardwrapper.board.piece_on(sm.mv.to).unwrap());
+				if !in_check && eval.score + captured_value + 200 <= alpha {
+					continue;
+				}
+			}
 
 			let (_, mut child_eval) = self.qsearch(&abort, &board_wrapper_cache, -beta, -alpha, ply + 1)?;
 
